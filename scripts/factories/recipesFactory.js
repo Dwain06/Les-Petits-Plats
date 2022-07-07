e5c4f5ac1ab21e0d5da083d371817c06a4cab73d
Dwain06 <dwain.mobile@gmail.com>
2022-07-07 16:13:59 +0200

    feat (cards): add ingredients, title and text to cards with factory pattern

--- scripts/factories/recipesFactory.js
+++ scripts/factories/recipesFactory.js
@@ -2,27 +2,74 @@ class RecipesFactory {
 
     constructor(data) {
         this.description = data.description;
+        this.name = data.name;
+        this.time = data.time;
+        this.ingredients = data.ingredients;
+    }
 
-        //DOM elements
-        this.divCol = document.createElement("div");
-
-        
-        //Set attributes and class
-        this.divCol.classList.add("col");
-
-        this.divCol.innerHTML =
-        `<div class="card shadow-sm">
-            <img class="card-img-top" src="https://dummyimage.com/420x225/dee2e6/6c757d.jpg" alt="" />
-            <div class="card-body">
-              <p class="card-text">${this.description}</p>
-            </div>
-          </div>`
-
-      
+    getIngredients() {
+      for (let i = 0; i < this.ingredients.length; i++) {
+        if (this.ingredients[i].unit == undefined) {
+          this.divIngredients.innerHTML +=
+          `<p><span class="fw-bold">${this.ingredients[i].ingredient} :</span> ${this.ingredients[i].quantity}</p>`;
+        } else {
+          this.divIngredients.innerHTML +=
+          `<p><span class="fw-bold">${this.ingredients[i].ingredient} :</span> ${this.ingredients[i].quantity} ${this.ingredients[i].unit}</p>`;
+        }
+      }
     }
 
     getRecipeCardDOM() {
-        return this.divCol;
+      //DOM elements of recipe card
+      const divCol = document.createElement("div");
+      const divCard = document.createElement("div");
+      const img = document.createElement("img");
+      const divCardBody = document.createElement("div");
+      const divCardTitle = document.createElement("div");
+      const divRow = document.createElement("div");
+      this.divIngredients = document.createElement("div");
+      const divCardText = document.createElement("p");
+      
+      //Set attributes and class for the CSS
+      divCol.classList.add("col");
+      divCard.classList.add("card", "shadow-sm");
+      img.classList.add("card-img-top");
+      img.setAttribute("src", "https://dummyimage.com/420x225/C7BEBE/6c757d.jpg");
+      divCardTitle.classList.add("card-title", "d-flex", "justify-content-between", "align-items-baseline");
+      divCardBody.classList.add("card-body", "bg-light");
+      divRow.classList.add("row");
+      this.divIngredients.classList.add("col", "ingredients");
+      divCardText.classList.add("col", "card-text");
+
+      //Text injected in HTML elements
+      divCardBody.innerHTML = 
+        `<div class="card-title d-flex justify-content-between align-items-baseline">
+        <h2 class="fw-normal">${this.name}</h2>
+        <p><i class="fa-regular fa-clock"></i>${this.time} min</p>
+        </div>`;
+      divCardText.textContent = `${this.description}`;
+
+      //Add created elements in the DOM
+      divCard.appendChild(img);
+      divCard.appendChild(divCardBody);
+      
+      divRow.appendChild(this.divIngredients);
+      divRow.appendChild(divCardText);
+      divCardBody.appendChild(divRow);
+
+      divCol.appendChild(divCard);
+
+      return {divCol};
     }
-   
+ 
+    static createRecipeCard(recipes) {
+      //Create section for each recipe in DOM
+      const divRecipes = document.getElementById("recipes");
+      recipes.forEach((recipe) => {
+          const recipesFactory = new RecipesFactory(recipe);
+          const recipeCardDOM = recipesFactory.getRecipeCardDOM();
+          divRecipes.appendChild(recipeCardDOM.divCol);
+          recipesFactory.getIngredients();
+      });
+  }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,12 +1,5 @@
 function init() {
-    const recipesSection = document.querySelector("#recipes");
-    console.log(recipes);
-
-    recipes.forEach((recipe) => {
-        const recipeCard = new RecipesFactory(recipe);
-        const recipeCardDOM = recipeCard.getRecipeCardDOM();
-        recipesSection.appendChild(recipeCardDOM);
-    });
+    RecipesFactory.createRecipeCard(recipesData);
 }
 
 init();
