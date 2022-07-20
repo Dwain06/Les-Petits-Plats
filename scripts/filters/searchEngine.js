b3fddcbcb66a1c0195bbc4e7bb41e5a083ccf88c
Dwain06 <dwain.mobile@gmail.com>
2022-07-20 17:42:15 +0200

    feat(algorithme2) create arrays of IDs

--- scripts/filters/searchEngine.js
+++ scripts/filters/searchEngine.js
@@ -1,56 +1,66 @@
 function search() {
-    let filtredData = researchInput(recipesData);
-    filtredData = researchIngredient(filtredData);
-    filtredData = researchAppliance(filtredData);
-    filtredData = researchUstensils(filtredData);
+    researchInput(recipesData);
+    researchIngredient(recipesData);
+    researchAppliance(recipesData);
+    researchUstensils(recipesData);
     init(filtredData);
 }
 
-
-function researchInput(datas) {
+function researchInput() {
+    let arrayIdInput = [];
     const value = searchInput.value;
-    if (value.length <= 2) return datas;
-
-    return datas.filter(recipe => {
-        return reciperToString(recipe).includes(value);
-    });
+    if (value.length > 2) {
+        recipesData.forEach(recipe => {
+            if (reciperToString(recipe).includes(value)) {
+                arrayIdInput.push(recipe.id);
+            }
+        });
+    }
+    console.log(arrayIdInput);
+    return arrayIdInput;
 }
 
 function reciperToString(recipe) {
     return recipe.name.toLowerCase() + ' ' + recipe.description.toLowerCase() + ' ' + recipe.ingredients.reduce((previous, current) => previous + ' ' + current.ingredient, "");
 }
 
-function researchIngredient(datas) {
-    return datas.filter(recipe => {
-        const ingNameList = recipe.ingredients.map(ing => ing.ingredient); // Liste des noms d'ingredient
+function researchIngredient() {
+    let arrayIdIng = [];
+    recipesData.forEach(recipe => {
+        const ingNameList = recipe.ingredients.map(ing => ing.ingredient); // Liste des noms d'ingredient de la recette
         const diff = researchInputTermsIng.filter(x => !ingNameList.includes(x)); // Liste de l'ensemble des tag n'étant pas dans ingNameList
-        
-        return diff.length === 0;
+        if (diff.length === 0) { //Si aucun tag n'est exclu d'une recette, l'id de la recette est ajoutée
+        arrayIdIng.push(recipe.id);
+        }
     });
+    console.log(arrayIdIng);
+    return arrayIdIng;
 }
 
-function researchAppliance(datas) {
-    if(researchInputTermsApp.length === 0){
-        return datas;
-    }
-
-    return datas.filter(recipe => {
-        return researchInputTermsApp.includes(recipe.appliance);
+function researchAppliance() {
+    let arrayIdApp = [];
+    recipesData.forEach(recipe => {
+        if (researchInputTermsApp.includes(recipe.appliance)){
+            arrayIdApp.push(recipe.id);
+        }
     });
+    console.log(arrayIdApp);
+    return arrayIdApp;
 }
 
-function researchUstensils(datas) {
-    return datas.filter(recipe => {
-        const ustensils = recipe.ustensils.map(ust => {
-            return capitalizeFirstLetter(ust); //On s'assure que la casse est confrome pour la recherche
-        });
+function researchUstensils() {
+    let arrayIdUst = [];
+    recipesData.forEach(recipe => {
+        const ustensils = recipe.ustensils.map(ust => capitalizeFirstLetter(ust)); //On s'assure que la casse est confrome pour la recherche
         const diff = researchInputTermsUst.filter(x => !ustensils.includes(x)); //Liste de l'ensemble des tag n'étant pas dans recipe.ustensils
-        
-        return diff.length === 0;
+        if (diff.length === 0) { //Si aucun tag n'est exclu d'une recette, l'id de la recette est ajoutée
+            arrayIdUst.push(recipe.id);
+        }
     });
+    console.log(arrayIdUst);
+    return arrayIdUst;
 }
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
-
