29e62602e3e3db28fcfa493798a8b9292d3aca95
Dwain06 <dwain.mobile@gmail.com>
2022-07-21 15:22:51 +0200

    feat(algorithme2) add main filter method

--- scripts/filters/searchEngine.js
+++ scripts/filters/searchEngine.js
@@ -1,109 +1,84 @@
-// let array1 = ["Lorem", "ipsum", "dolor"]
-// let array2 = ["Lorem", "ipsum", "quick", "brown", "foo"]
-// let array3 = ["Jumps", "Over", "Lazy", "Lorem"]
-// let array4 = [1337, 420, 666, "Lorem"]
-// let data = [array1, array2, array3, array4]
-// console.log(data);
-// let result = data.reduce((a, b) => a.filter(c => b.includes(c)));
-
-// console.log(result);
+let arrayId = [];
+let arrayIdInput = [];
+let arrayIdIng = [];
+let arrayIdApp = [];
+let arrayIdUst = [];
 
 function search() {
     let filtredData = [];
-    researchInput();
-    researchIngredient();
-    researchAppliance();
-    researchUstensils();
     
-    let arrayId = [];
-
-    if (researchInput() !== undefined) {
-        arrayId.push(researchInput());
-    }
-
-    if (researchIngredient() !== undefined) {
-        arrayId.push(researchIngredient());
-    }
-    console.log(arrayId);
-
-    arrayId = arrayId.reduce((a, b) => a.filter(c => b.includes(c)));
-
-    console.log(arrayId);
-
-    arrayId.forEach(idX => {
-        recipesData.forEach(recipe => {
-            if (recipe.id == idX) {
-                filtredData.push(recipe);
-            }
+    //Si aucun terme de recherche n'est entré, on initialise le tableau de toutes les recettes
+    if (researchInputTermsUst.length === 0 && researchInputTermsIng.length === 0 && researchInputTermsApp.length === 0 && searchInput.value.length <= 2) {
+        init(recipesData);
+    } else {
+        arrayId = [];
+        if (searchInput.value.length > 2) researchInput();
+        if (researchInputTermsIng.length !== 0) researchIngredient();
+        if (researchInputTermsApp.length !== 0) researchAppliance();
+        if (researchInputTermsUst.length !== 0) researchUstensils();
+        
+        //On cherche les id identiques présents dans chacun des tableaux
+        arrayId = arrayId.reduce((a, b) => a.filter(c => b.includes(c)));
+    
+        //On retourne un tableau de recttes correspondantes aux id trouvés précédemment
+        arrayId.forEach(idX => {
+            recipesData.forEach(recipe => {
+                if (recipe.id == idX) {
+                    filtredData.push(recipe);
+                }
+            });
         });
-    });
 
-    console.log(filtredData);
-
-    init(filtredData);
+        init(filtredData);
+    }
 }
 
-let arrayIdInput = [];
 function researchInput() {
     arrayIdInput = [];
-    const value = searchInput.value;
-    if (value.length > 2) {
-        recipesData.forEach(recipe => {
-            if (reciperToString(recipe).includes(value)) {
-                arrayIdInput.push(recipe.id);
-            }
-        });
-    }
-    if (arrayIdInput.length !== 0) {
-        return arrayIdInput;
-    }
+    recipesData.forEach(recipe => {
+        if (reciperToString(recipe).includes(searchInput.value)) {
+            arrayIdInput.push(recipe.id);
+        }
+    });
+    arrayId.push(arrayIdInput);
 }
 
 function reciperToString(recipe) {
     return recipe.name.toLowerCase() + ' ' + recipe.description.toLowerCase() + ' ' + recipe.ingredients.reduce((previous, current) => previous + ' ' + current.ingredient, "");
 }
 
-let arrayIdIng = [];
 function researchIngredient() {
     arrayIdIng = [];
-    if (researchInputTermsIng.length !== 0){
-        recipesData.forEach(recipe => {
-            const ingNameList = recipe.ingredients.map(ing => capitalizeFirstLetter(ing.ingredient)); // Liste des noms d'ingredient de la recette
-            const diff = researchInputTermsIng.filter(x => !ingNameList.includes(x)); // Liste de l'ensemble des tag n'étant pas dans ingNameList
-            if (diff.length === 0) { //Si aucun tag n'est exclu d'une recette, l'id de la recette est ajoutée
-            arrayIdIng.push(recipe.id);
-            }
-        });
-    }
-    if (arrayIdIng.length !== 0) {
-        return arrayIdIng;
-    }
+    recipesData.forEach(recipe => {
+        const ingNameList = recipe.ingredients.map(ing => capitalizeFirstLetter(ing.ingredient)); // Liste des noms d'ingredient de la recette
+        const diff = researchInputTermsIng.filter(x => !ingNameList.includes(x)); // Liste de l'ensemble des tag n'étant pas dans ingNameList
+        if (diff.length === 0) { //Si aucun tag n'est exclu d'une recette, l'id de la recette est ajoutée
+        arrayIdIng.push(recipe.id);
+        }
+    });
+    arrayId.push(arrayIdIng);
 }
 
-let arrayIdApp = [];
 function researchAppliance() {
-    if (researchInputTermsApp.length !== 0){
-        recipesData.forEach(recipe => {
-            if (researchInputTermsApp.includes(recipe.appliance)){
-                arrayIdApp.push(recipe.id);
-            }
-        });
-    }
-    return arrayIdApp;
+    arrayIdApp = [];
+    recipesData.forEach(recipe => {
+        if (researchInputTermsApp.includes(recipe.appliance)){
+            arrayIdApp.push(recipe.id);
+        }
+    });
+    arrayId.push(arrayIdApp);
 }
 
-let arrayIdUst = [];
 function researchUstensils() {
-    if (researchInputTermsUst.length !== 0){
-        recipesData.forEach(recipe => {
-            const ustensils = recipe.ustensils.map(ust => capitalizeFirstLetter(ust)); //On s'assure que la casse est confrome pour la recherche
-            const diff = researchInputTermsUst.filter(x => !ustensils.includes(x)); //Liste de l'ensemble des tag n'étant pas dans recipe.ustensils
-            if (diff.length === 0) { //Si aucun tag n'est exclu d'une recette, l'id de la recette est ajoutée
-                arrayIdUst.push(recipe.id);
-            }
-        });
-    }
-    return arrayIdUst;
+    arrayIdUst = [];
+    recipesData.forEach(recipe => {
+        const ustensils = recipe.ustensils.map(ust => capitalizeFirstLetter(ust)); //On s'assure que la casse est confrome pour la recherche
+        const diff = researchInputTermsUst.filter(x => !ustensils.includes(x)); //Liste de l'ensemble des tag n'étant pas dans recipe.ustensils
+        if (diff.length === 0) { //Si aucun tag n'est exclu d'une recette, l'id de la recette est ajoutée
+            arrayIdUst.push(recipe.id);
+        }
+    });
+    arrayId.push(arrayIdUst);
 }
 
 function capitalizeFirstLetter(string) {
